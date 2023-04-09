8fd4c656b1fb692a42aa6b2e19ab5034f6b7a2ab
MauroMA-99 <mauro2017pre@gmail.com>
2023-04-09 00:14:03 -0500

    Matplotlib 2

---
+++ charts/prueba.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+# crea tu gráfica aquí
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+#plt.show()
+# guarda la gráfica en formato PNG
+plt.savefig('nombre_de_tu_archivo.png')
